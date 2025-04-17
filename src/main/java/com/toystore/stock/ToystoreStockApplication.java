2ad0c506c4aafe0438bca0c3c1cb0ed922b8e2c8
fakynno <fakynno@gmail.com>
2025-04-16 23:49:17 -0300

    importe inicial do projeto e estrutura de pastas

---
+++ src/main/java/com/toystore/stock/ToystoreStockApplication.java
@@ -0,0 +1,13 @@
+package com.toystore.stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToystoreStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToystoreStockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toystore/stock/ToystoreStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toystore.stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToystoreStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
